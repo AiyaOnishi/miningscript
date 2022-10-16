67a3a1bb0eb4c4e4e10dc5ee4b20ff04f8bb72ad
AiyaOnishi <96187697+AiyaOnishi@users.noreply.github.com>
2022-10-16 19:06:52 -0400

    Update gpuminersetup.sh

--- gpuminersetup.sh
+++ gpuminersetup.sh
@@ -15,6 +15,7 @@ git clone https://github.com/xmrig/xmrig-cuda.git \
 mkdir xmrig-cuda/build && cd xmrig-cuda/build \
 cmake .. -DCUDA_LIB=/usr/local/cuda/lib64/stubs/libcuda.so -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda \
 make -j$(nproc) \
+wget https://raw.githubusercontent.com/AiyaOnishi/miningscript/main/config.json \
 sudo mv /xmrig/build/xmrig /home/root/ \
 sudo mv /xmrig-cuda/build/libxmrig-cuda.so /home/root/ \
 cd /home/root/ \
