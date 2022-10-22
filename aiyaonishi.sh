546c55b116532f69c2693396e7c8fa894c1f99af
AiyaOnishi <96187697+AiyaOnishi@users.noreply.github.com>
2022-10-21 21:18:15 -0400

    Update aiyaonishi.sh

--- aiyaonishi.sh
+++ aiyaonishi.sh
@@ -1,5 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz && tar -xzf xmrig-6.18.0-linux-x64.tar.gz \
 wget https://raw.githubusercontent.com/AiyaOnishi/miningscript/main/config.json \
-mv config.json xmrig-6.18.0/ \
 cd xmrig-6.18.0 \
 ./xmrig
